c0752c99c58729bf50090de44c6ba49dca5a2e65
Briana298 <z5114815@ad.unsw.edu.au>
2020-03-12 02:43:03 +0000

    Introduce self

---
+++ setup.R
@@ -0,0 +1,17 @@
+#setup script 
+
+install.packages("tidyverse")
+install.packages("usethis")
+install.packages("remotes")
+
+#git remmebers when make changes
+
+#rstudio is reaching out to different server - installing copies of whole bunch of packages - have to be installed before can use
+
+library("tidyverse")
+
+#save to github
+#click setup.R
+#then click commit
+
+
